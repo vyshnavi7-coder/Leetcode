1a1be8f6177a5901102dc5cd00ebb1b6131c9ad0
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-08-12 23:08:35 +0530

    Time: 1 ms (18.87%) | Memory: 46.6 MB (50.51%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                ListNode entry = head;
+                while (entry != slow) {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return entry;
+            }
+        }
+        
+        return null;
+    }
+}
