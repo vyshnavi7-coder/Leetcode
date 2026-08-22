c3a434644a14a0b1e71d10d647fa3f088e82389b
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-08-22 21:47:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (79.33%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int digitSum = 0;
+        int digitProduct = 1;
+        int temp = n;
+        
+        while (temp > 0) {
+            int digit = temp % 10;
+            digitSum += digit;
+            digitProduct *= digit;
+            temp /= 10;
+        }
+        
+        return n % (digitSum + digitProduct) == 0;
+    }
+}
