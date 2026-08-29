d7a81f409c9f7178853de36eb22a99155ac1c679
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-08-29 14:05:19 +0530

    Time: 31 ms (60.41%) | Memory: 55.4 MB (20.65%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class StockSpanner {
+    private Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
