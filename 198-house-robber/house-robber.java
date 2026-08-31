3cfd5d1222b9118c15ab45da5cb57212c38c84e7
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-08-31 16:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (94.68%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        
+        int prev2 = 0;
+        int prev1 = 0;
+        
+        for (int num : nums) {
+            int current = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return prev1;
+    }
+}
