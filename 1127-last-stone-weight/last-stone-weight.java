1d53ce0a162795764519fcefae0fc3dd30390054
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-09-14 22:46:37 +0530

    Time: 1 ms (99.65%) | Memory: 43 MB (32.75%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+        
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+            
+            if (stone1 != stone2) {
+                maxHeap.add(stone1 - stone2);
+            }
+        }
+        
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
+    }
+}
