89a7ddcdee02d92edc10d1cb6a0be5e1efa6be5b
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-09-15 21:15:09 +0530

    Time: 191 ms (31.91%) | Memory: 112.2 MB (8.36%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+    private PriorityQueue<Integer> small;
+    private PriorityQueue<Integer> large;
+
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        small.offer(num);
+        large.offer(small.poll());
+        
+        if (small.size() < large.size()) {
+            small.offer(large.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if (small.size() > large.size()) {
+            return small.peek();
+        }
+        return (small.peek() + large.peek()) / 2.0;
+    }
+}
