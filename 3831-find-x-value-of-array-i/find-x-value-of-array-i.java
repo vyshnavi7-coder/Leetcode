7aa47af0d703edd185df2b4ecd72c8156b218901
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-09-21 22:40:37 +0530

    Time: 15 ms (32.69%) | Memory: 92 MB (82.69%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] ans = new long[k];
+        long[] dp = new long[k];
+        
+        for (int num : nums) {
+            long[] newDp = new long[k];
+            int numMod = num % k;
+            
+            newDp[numMod] = 1;
+            
+            for (int i = 0; i < k; ++i) {
+                int newMod = (int) (1L * i * numMod % k);
+                newDp[newMod] += dp[i];
+            }
+            
+            for (int i = 0; i < k; ++i) {
+                ans[i] += newDp[i];
+            }
+            
+            dp = newDp;
+        }
+        
+        return ans;
+    }
+}
