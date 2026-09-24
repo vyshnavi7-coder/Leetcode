9767fbad9d0a1195474984628476cfa1680767a2
VYSHNAVI CHINTHALA <chinthalavyshnavi7@gmail.com>
2026-09-24 14:53:44 +0530

    Time: 4 ms (98.20%) | Memory: 102.2 MB (29.47%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        int target = totalSum - x;
+        if (target < 0) {
+            return -1;
+        }
+        if (target == 0) {
+            return nums.length;
+        }
+        
+        int left = 0;
+        int currentSum = 0;
+        int maxLen = -1;
+        
+        for (int right = 0; right < nums.length; right++) {
+            currentSum += nums[right];
+            
+            while (currentSum > target && left <= right) {
+                currentSum -= nums[left++];
+            }
+            
+            if (currentSum == target) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        
+        return maxLen == -1 ? -1 : nums.length - maxLen;
+    }
+}
